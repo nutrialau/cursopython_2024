9d8fb3ea1b4450891afe615c7b72cea37f7b68f8
Laura Aviles <avilau@hotmail.com>
2024-06-04 20:07:39 -0300

    conecte el git con la consola de py

---
+++ clase4_junio.py
@@ -0,0 +1,7 @@
+""" alt+shift+a= comentario (no se va a ejecutar en el codigo) """
+""" "#" tambien sirve para comentar, pero solo para comentar en una linea, las comillas es multilinea """#escribe en verde
+print(1+24) #"print+()" ejecuta algo ya sea suma resta
+""" print sin parentesis imprime lo arrojado""" 
+# es importante comentar cada vez que nosotros cambiemos algo para explicarlo. ayuda a otra persona a entender y es parte de las bpp 
+""" snake case es la convencion que se usa en py, la nomenclatura que obliga a escribir todo en minuscula y la separacion con guiones bajos: "hola_mundo". camel case significa que se escribe todo junto en minuscula y de ser dos o mas palabras, la segunda (tercera, etc) se aplica la mayuscula: "holaMundo"  """
+#para conectar el repo con vsc, "git init" en la consola
